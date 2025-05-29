c2c5c377f1fa3676a9b7e46525afbaa7aff3b13a
Abhishek Jindal <abhishekjindal13@gmail.com>
2025-05-29 15:19:44 +0000

    Color Picker with useStateWithHistory Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./styles.css";
 import PostCard from "./PostCard";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import ColorPicker from "./components/ColorPicker";
 export default function App() {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <PostCard postId="1" />
-      </div>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Hello CodeSandbox</h1>
+          <PostCard postId="1" />
+        </div>
+      </QueryClientProvider>
+      <ColorPicker />
+    </>
   );
 }
